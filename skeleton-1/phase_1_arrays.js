3de5a9eb9f0fe87a92b7c09b7c308c42c131ee02
Cornell Bethea <betheacornellb@gmail.com>
2023-01-04 15:12:17 -0500

    forEach

---
+++ skeleton-1/phase_1_arrays.js
@@ -0,0 +1,15 @@
+
+
+Array.prototype.uniq = function() {
+    let newArr = [];
+    for (let i = 0; i < this.length; i++){
+        
+    }
+
+}
+
+// {0:1, 1:2, 2:3, 3:4}
+    
+
+    let nums = [1,1,2,2,3,3,3,4,4]
+console.log(nums.uniq)
